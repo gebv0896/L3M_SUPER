4c03a2d14725829d57cc94cb9908884fc4d80361
GABRIEL <GABRIEL@GABRIEL>
2019-08-14 22:44:24 -0600

    Se agregó el Entity Framework en referencias y se creó la clase modelo "Rol"

---
+++ L3M Super/Models/Rol.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace L3M_Super.Models
+{
+    public class Rol
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string nombreRol { get; set; }
+        public string descripcionRol { get; set; }
+    }
+
+    class RolesDbContext : DbContext
+    {
+        public DbSet<Rol> Roles { get; set; }
+    }
+
+
+}
