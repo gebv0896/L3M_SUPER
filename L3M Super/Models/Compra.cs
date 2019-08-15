63b4b8a5a19980f7294e72f267d22e45f269838b
GABRIEL <GABRIEL@GABRIEL>
2019-08-14 23:31:25 -0600

    Clase Compra y Trabajador

---
+++ L3M Super/Models/Compra.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace L3M_Super.Models
+{
+    public class Compra
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string descripcionCompra { get; set; }
+        public string fechaRealCompra { get; set; }
+        public string fechaRegistroCompra { get; set; }
+        public string proveedor { get; set; }
+        public string fotoCompra { get; set; }
+        public string sucursalRegistra { get; set; }
+
+    }
+
+    class ComprasDbContext : DbContext
+    {
+        public DbSet<Compra> Compras { get; set; }
+    }
+
+
+}
---
+++ L3M Super/Models/Trabajador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace L3M_Super.Models
+{
+    public class Trabajador
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int cedula { get; set; }
+        public string nombreCompleto { get; set; }
+        public string fechaNacimiento { get; set; }
+        public string fechaIngreso { get; set; }
+        public string sucursal { get; set; }
+        public int salarioHora { get; set; }
+
+    }
+
+    class TrabajadoresDbContext : DbContext
+    {
+        public DbSet<Trabajador> Trabajadores { get; set; }
+    }
+
+
+}
+}
