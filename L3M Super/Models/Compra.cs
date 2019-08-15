5ee6813287c9721de987091a5e801db0bbb930fa
GABRIEL <GABRIEL@GABRIEL>
2019-08-14 23:39:26 -0600

    Cambio de Key en la clase Compra

--- L3M Super/Models/Compra.cs
+++ L3M Super/Models/Compra.cs
@@ -10,12 +10,13 @@ namespace L3M_Super.Models
 {
     public class Compra
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        
         public string descripcionCompra { get; set; }
         public string fechaRealCompra { get; set; }
         public string fechaRegistroCompra { get; set; }
         public string proveedor { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string fotoCompra { get; set; }
         public string sucursalRegistra { get; set; }
 
