56a51a66049a3690e26fd5caa2e7d9a586419502
gaburolo <gaburolo@live.com>
2019-08-14 22:49:48 -0600

    Sucursal agregada

---
+++ L3M Super/Models/Proveedor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace L3M_Super.Models
+{
+    public class Proveedor
+    {
+    }
+}
---
+++ L3M Super/Models/Sucursal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace L3M_Super.Models
+{
+    public class Sucursal
+    {
+        [Key]
+        [DatabaseGerated(DatabaseGeneratedOption.None)]
+        public string nombreSucursal { get; set; }
+        public string direccionSurcusal { get; set; }
+        public int telefonoSucursal { get; set; }
+        public string administradorSucursal { get; set; }
+
+    }
+    class SucursalesDbContext : DbContext
+    {
+        public DbSet<Sucursal> Sucursales { get; set; }
+    }
+}
