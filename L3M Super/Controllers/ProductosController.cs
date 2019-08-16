b3bd74fe8ab223a2402bcca59368411579c12411
GABRIEL <GABRIEL@GABRIEL>
2019-08-15 19:50:32 -0600

    Controller Productos

---
+++ L3M Super/Controllers/ProductosController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using L3M_Super.Models;
+
+namespace L3M_Super.Controllers
+{
+    public class ProductosController : ApiController
+    {
+        private ProductosDbContext db = new ProductosDbContext();
+
+        // GET: api/Productos
+        public IQueryable<Producto> GetProductos()
+        {
+            return db.Productos;
+        }
+
+        // GET: api/Productos/5
+        [ResponseType(typeof(Producto))]
+        public IHttpActionResult GetProducto(int id)
+        {
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(producto);
+        }
+
+        // PUT: api/Productos/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProducto(Producto producto)
+        {
+            int id = producto.codigoBarraProducto;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != producto.codigoBarraProducto)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(producto).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Productos
+        [ResponseType(typeof(Producto))]
+        public IHttpActionResult PostProducto(Producto producto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Productos.Add(producto);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductoExists(producto.codigoBarraProducto))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = producto.codigoBarraProducto }, producto);
+        }
+
+        // DELETE: api/Productos/5
+        [ResponseType(typeof(Producto))]
+        public IHttpActionResult DeleteProducto(int id)
+        {
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            db.Productos.Remove(producto);
+            db.SaveChanges();
+
+            return Ok(producto);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductoExists(int id)
+        {
+            return db.Productos.Count(e => e.codigoBarraProducto == id) > 0;
+        }
+    }
+}
