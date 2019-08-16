d6a3a2b4795143fbd68a0ec950b18dc77b98f297
GABRIEL <GABRIEL@GABRIEL>
2019-08-15 20:09:51 -0600

    Controller Roles

---
+++ L3M Super/Controllers/RolesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using L3M_Super.Models;
+
+namespace L3M_Super.Controllers
+{
+    public class RolesController : ApiController
+    {
+        private RolesDbContext db = new RolesDbContext();
+
+        // GET: api/Roles
+        public IQueryable<Rol> GetRoles()
+        {
+            return db.Roles;
+        }
+
+        // GET: api/Roles/5
+        [ResponseType(typeof(Rol))]
+        public IHttpActionResult GetRol(string id)
+        {
+            Rol rol = db.Roles.Find(id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(rol);
+        }
+
+        // PUT: api/Roles/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutRol(Rol rol)
+        {
+            string id = rol.nombreRol;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != rol.nombreRol)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(rol).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RolExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Roles
+        [ResponseType(typeof(Rol))]
+        public IHttpActionResult PostRol(Rol rol)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Roles.Add(rol);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (RolExists(rol.nombreRol))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = rol.nombreRol }, rol);
+        }
+
+        // DELETE: api/Roles/5
+        [ResponseType(typeof(Rol))]
+        public IHttpActionResult DeleteRol(string id)
+        {
+            Rol rol = db.Roles.Find(id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            db.Roles.Remove(rol);
+            db.SaveChanges();
+
+            return Ok(rol);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RolExists(string id)
+        {
+            return db.Roles.Count(e => e.nombreRol == id) > 0;
+        }
+    }
+}
